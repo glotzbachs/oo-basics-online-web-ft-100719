a75ca42202f23dafd1546093d96c5f1da9610275
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-15 02:29:29 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,14 @@
 # Make your shoe class here!
 
 class Shoe
+  attr_accessor :brand,:color,:size,:material,:condition
   
-  
+  def initialize(brand)
+    @brand=brand
+  end
+  def cobble
+    puts "Shoe has been repaired"
+    :condition="new"
+  end
   
 end
